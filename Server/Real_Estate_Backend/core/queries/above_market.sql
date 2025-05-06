5f48f2d72ac860b8f48e50c617e027cd237c45c5
lakshaysawhney <lsawhney07@gmail.com>
2025-05-07 01:29:24 +0530

    Queries update

--- Server/Real_Estate_Backend/core/queries/above_market.sql
+++ Server/Real_Estate_Backend/core/queries/above_market.sql
@@ -7,11 +7,12 @@ FROM Rentals r
 JOIN Properties p ON r.property_id = p.property_id
 JOIN (
     SELECT
-        city,
-        neighbourhood,
-        avg_rent_per_sqft * property_size AS market_estimated_rent
-    FROM Properties
-    JOIN Market_Trends mt ON Properties.city = mt.city AND Properties.neighbourhood = mt.neighbourhood
+        p.city,
+        p.neighbourhood,
+        mt.avg_rent_per_sqft * MAX(p.property_size) AS market_estimated_rent
+    FROM Properties p
+    JOIN Market_Trends mt ON p.city = mt.city AND p.neighbourhood = mt.neighbourhood
+    GROUP BY p.city, p.neighbourhood, mt.avg_rent_per_sqft
 ) mt ON p.city = mt.city AND p.neighbourhood = mt.neighbourhood
 WHERE r.monthly_rent > mt.market_estimated_rent
 
