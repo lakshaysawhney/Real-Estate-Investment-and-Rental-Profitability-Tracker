7ba5b586f099abaa654c353b1af6a60bc15b6828
lakshaysawhney <lsawhney07@gmail.com>
2025-05-07 01:40:13 +0530

    Fixes

--- Server/Real_Estate_Backend/core/queries/cashflow_monthly.sql
+++ Server/Real_Estate_Backend/core/queries/cashflow_monthly.sql
@@ -1,9 +1,26 @@
 SELECT
-    p.property_id, p.address,
-    (NVL(SUM(r.monthly_rent), 0) - (NVL(SUM(e.amount), 0) / 12)) AS cash_flow
+    p.property_id,
+    p.address,
+    NVL(r.total_rent, 0) * 12 - NVL(e.total_expense, 0) AS cash_flow
 FROM Properties p
-LEFT JOIN Rentals r ON p.property_id = r.property_id
-LEFT JOIN Expenses e ON p.property_id = e.property_id
-GROUP BY p.property_id, p.address
+LEFT JOIN (
+    SELECT property_id, SUM(monthly_rent) AS total_rent
+    FROM Rentals
+    GROUP BY property_id
+) r ON p.property_id = r.property_id
+LEFT JOIN (
+    SELECT property_id, SUM(amount) AS total_expense
+    FROM Expenses
+    GROUP BY property_id
+) e ON p.property_id = e.property_id
+
+
+-- SELECT
+--     p.property_id, p.address,
+--     (NVL(SUM(r.monthly_rent), 0) - (NVL(SUM(e.amount), 0) / 12)) AS cash_flow
+-- FROM Properties p
+-- LEFT JOIN Rentals r ON p.property_id = r.property_id
+-- LEFT JOIN Expenses e ON p.property_id = e.property_id
+-- GROUP BY p.property_id, p.address
 
 -- NVL is Oracle's version of COALESCE
--- Server/Real_Estate_Backend/core/queries/roe.sql
+++ Server/Real_Estate_Backend/core/queries/roe.sql
@@ -1,12 +1,23 @@
 SELECT
     p.property_id,
     p.address,
-    ROUND(((COALESCE(SUM(r.monthly_rent) * 12, 0) - COALESCE(SUM(e.amount), 0)) / 
-    NULLIF(p.current_value, 0)) * 100, 2) AS ROE
+    ROUND(
+        (
+            COALESCE(r.total_rent, 0) * 12 - COALESCE(e.total_expense, 0)
+        ) / t.amount * 100, 2
+    ) AS ROE
 FROM Properties p
-LEFT JOIN Rentals r ON p.property_id = r.property_id
-LEFT JOIN Expenses e ON p.property_id = e.property_id
-GROUP BY p.property_id, p.address, p.current_value
+JOIN Transactions t ON p.property_id = t.property_id AND t.type = 'purchase'
+LEFT JOIN (
+    SELECT property_id, SUM(monthly_rent) AS total_rent
+    FROM Rentals
+    GROUP BY property_id
+) r ON p.property_id = r.property_id
+LEFT JOIN (
+    SELECT property_id, SUM(amount) AS total_expense
+    FROM Expenses
+    GROUP BY property_id
+) e ON p.property_id = e.property_id
 
 -- ROE = For current market value of this property, how much returns the investor is making annually
 -- ROE = (Net income / Current Value of Property) * 100
